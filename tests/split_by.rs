f8144980687b4abe8346c26a6e3a837be2d286dd
Geoff Shannon <geoffpshannon@gmail.com>
2017-11-30 00:29:08 -0600

    Remove reverse order test

--- tests/split_by.rs
+++ tests/split_by.rs
@@ -11,13 +11,3 @@ fn test_normal_order() {
     assert_eq!(vec![4, 5, 6],
                last_three.collect::<Vec<u8>>());
 }
-
-#[test]
-fn test_reverse_order() {
-    let nums = vec![1, 2, 3, 4, 5, 6];
-    let (first_three, last_three) = split_by(nums.into_iter(), |&i| i < 4 );
-    assert_eq!(vec![4, 5, 6],
-               last_three.collect::<Vec<u8>>());
-    assert_eq!(vec![1, 2, 3],
-               first_three.collect::<Vec<u8>>());
-}
