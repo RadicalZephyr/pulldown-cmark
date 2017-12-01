a4d64bfed101e302a33415d0616f86c559caa2e2
Geoff Shannon <geoffpshannon@gmail.com>
2017-11-30 18:57:24 -0600

    Spruce up tests or something

--- tests/ast.rs
+++ tests/ast.rs
@@ -2,7 +2,7 @@ extern crate pulldown_cmark;
 
 use std::fmt::Debug;
 
-use pulldown_cmark::{Content, Parser, Tag, Node};
+use pulldown_cmark::{Content, Parser, Event, Tag, Node};
 
 fn print_iter<I, D>(iter: I)
     where
@@ -32,39 +32,60 @@ fn ast_contents_first_node() {
 
 }
 
-// // #[test]
-// fn first_child_of_first_node() {
-//     let original = r##"# [link](/to/here)
-// "##;
+#[test]
+fn first_child_of_first_node() {
+    let original = r##"# [link](/to/here)
+    "##;
 
-//     let p = Parser::new(&original);
+    let p = Parser::new(&original);
+    let v: Vec<_>  = p.collect();
+    print_iter(v.iter());
+    println!("\n\n");
 
-//     let mut content = Content::new(Box::new(p));
-//     let content_head = content.next();
+    let mut content = Content::new(v.into_iter());
 
-//     let mut children = content_head.unwrap().content;
-//     let children_head = children.next();
+    match content.next() {
+        Some(Node::Block(_, mut content)) => {
+            println!("{:?}", content);
+            match content.next() {
+                Some(Node::Block(first_child, _)) => {
 
-//     assert!(children_head.is_some());
-//     assert_eq!(&Tag::Link("/to/here".into(), "".into()),
-//                children_head.unwrap().tag());
+                    assert_eq!(Tag::Link("/to/here".into(), "".into()), first_child)
+                },
+                Some(Node::Item(e)) => assert_eq!(Event::SoftBreak, e),
+                None => assert!(false),
+            }
+        },
+        _ => assert!(false),
+    }
+}
 
-// }
+#[test]
+fn ast_contents_second_node() {
+    let original = r##"# [link](/to/here)
 
-// // #[test]
-// fn past_first_child() {
-//     let original = r##"# [link](/to/here)
-// "##;
+Hello
+"##;
 
-//     let p = Parser::new(&original);
-//     let mut content = Content::new(Box::new(p));
-//     let head = content.next();
-//     assert!(head.is_some());
+    let p = Parser::new(&original);
+    let v: Vec<_>  = p.collect();
+    print_iter(v.iter());
+    println!("\n\n");
 
-//     let second = content.next();
-//     assert!(second.is_some());
+    let mut content = Content::new(v.into_iter());
+    println!("{:?}\n\n", content);
+    let content_head = {
+        content.next(); // Skip the first Node
+        println!("{:?}\n\n", content);
+        content.next()
+    };
+    println!("{:?}\n\n", content);
+    assert!(content_head.is_some());
+    match content_head {
+        Some(Node::Block(tag, _)) =>
+            assert_eq!(Tag::Paragraph, tag),
+        _ => assert!(false)
+    }
 
-//     // assert_eq!(&Tag::Link("/to/here".into(), "".into()),
-//     //            content_head.unwrap().tag());
 
-// }
+}
